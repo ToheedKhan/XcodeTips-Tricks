6d3cbc4cc9780677aadbf6ad32513f653d123450
Toheed Jahan Khan <toheed04@gmail.com>
2022-12-22 13:08:42 +0530

    Added Sample Project and .yml file

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Toheed Jahan Khan on 22/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
